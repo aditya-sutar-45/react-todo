92012b962eab585c71a882a328f5c5e1ba372071
Aditya Sutar <aditya1.sutar@outlook.com>
2025-02-18 17:45:58 +0530

    changed the list route on homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router";
+
 export default function Home() {
   return (
     <>
       <h1>TODO or NOT TODO</h1>
       <p style={{fontStyle: "italic"}}>thus begins thy journey of productivity...</p>
-      <a href="/lists">go to my lists...</a>
+      <Link to="/lists">Go to Lists</Link>
     </>
   );
 }
