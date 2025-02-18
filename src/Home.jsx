0521e668c0db8f5e044a22309db1b9b8cb6a06ca
Aditya Sutar <aditya1.sutar@outlook.com>
2025-02-18 16:46:33 +0530

    changes to home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>Todo List</h1>
+      <h1>TODO or NOT TODO</h1>
+      <p style={{fontStyle: "italic"}}>thus begins thy journey of productivity...</p>
       <a href="/react-todo/lists">go to my lists...</a>
     </>
   );
